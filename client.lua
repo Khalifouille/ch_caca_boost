b447cda1f89037f6fd93fc4de130637c83e99d72
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-07-20 20:38:47 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,18 @@
+local isBoosted = false
+
 RegisterCommand("sevider", function()
     local playerPed = PlayerPedId()
 
-    RequestAnimDict("missfbi3ig_0")
-    while not HasAnimDictLoaded("missfbi3ig_0") do
-        Wait(10)
+    if isBoosted then
+        TriggerEvent("chat:addMessage", {
+            color = {255, 0, 0},
+            args = {"Système", "⛔ Tu es déjà boosté ! Patiente la fin de l'effet."}
+        })
+        return
     end
+
+    RequestAnimDict("missfbi3ig_0")
+    while not HasAnimDictLoaded("missfbi3ig_0") do Wait(10) end
 
     TaskPlayAnim(playerPed, "missfbi3ig_0", "shit_loop_trev", 8.0, -8, -1, 1, 0, false, false, false)
 
@@ -13,7 +21,6 @@ RegisterCommand("sevider", function()
 
     TriggerEvent("chat:addMessage", {
         color = {255, 255, 0},
-        multiline = true,
         args = {"Système", "💩 Tu es en train de te vider... Patiente une minute."}
     })
 
@@ -23,22 +30,23 @@ RegisterCommand("sevider", function()
     FreezeEntityPosition(playerPed, false)
     SetPlayerControl(PlayerId(), true, 0)
 
-
-    local originalSprintMultiplier = GetRunSprintMultiplierForPlayer(PlayerId())
-    SetRunSprintMultiplierForPlayer(PlayerId(), 1.5)
+    isBoosted = true
+    RequestAnimSet("move_m@quick")
+    while not HasAnimSetLoaded("move_m@quick") do Wait(10) end
+    SetPedMovementClipset(playerPed, "move_m@quick", 5.0)
 
     TriggerEvent("chat:addMessage", {
         color = {0, 255, 0},
-        multiline = true,
         args = {"Système", "💨 Tu cours maintenant 1.5x plus vite pendant 5 minutes !"}
     })
 
     Wait(5 * 60000)
-    SetRunSprintMultiplierForPlayer(PlayerId(), 1.0)
+
+    ResetPedMovementClipset(playerPed, 1.0)
+    isBoosted = false
 
     TriggerEvent("chat:addMessage", {
         color = {255, 0, 0},
-        multiline = true,
         args = {"Système", "⏳ Ton boost de vitesse est terminé."}
     })
 end)
